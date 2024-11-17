962dcade72c4d5d2eb987fa4fcffd2fab0852eae
Pratik <badhepd@gmail.com>
2024-11-17 12:46:04 +0000

    fix: :bug: fix env

--- apps/api/config/settings.py
+++ apps/api/config/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": os.getenv("DB_ENGINE"),
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PWD"),
